572f015fce802f20c6de08955989a98254eabce8
NikitaDobry27 <bazhenov.nikita27@gmail.com>
2023-01-26 18:52:42 +0100

    Update proxy_checker.py

--- proxy_checker.py
+++ proxy_checker.py
@@ -16,8 +16,8 @@ def make_request(url, proxy):
 
 url = "https://checker.soax.com/api/ipinfo"
 
-
-proxies = ["http://YT7fBfPlVx5S09VH:wifi;na;;;@proxy.soax.com:{}".format(port) for port in range(9000, 9000 + int(input("Insert desired number of port: ")))]
+num_of_ports = int(input("Insert desired number of port: "))
+proxies = ["http://YT7fBfPlVx5S09VH:wifi;us;;;@proxy.soax.com:{}".format(port) for port in range(9000, 9000 + num_of_ports)]
 
 
 with concurrent.futures.ThreadPoolExecutor() as executor:
@@ -34,17 +34,20 @@ with concurrent.futures.ThreadPoolExecutor() as executor:
     else:
       result_counts[result] = 1
   
+
   uniq_results_count = 0
   for result, count in result_counts.items():
     if count >= 1:
       uniq_results_count += 1
     
   duplicated_results = len(results) - uniq_results_count
-  
+  errors_occured = num_of_ports - len(results)
   
   for result, count in result_counts.items():
     print("{}: {}".format(result, count))
   print("Total requests: {}".format(len(results)))
   print("Uniq IPs: {}".format(uniq_results_count))
   print("Duplicated IPs: {}".format(duplicated_results))
+  print("Errors: {}".format(errors_occured))
+ 
   
